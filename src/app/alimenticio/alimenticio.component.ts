819c55ad6d49b61f96ae704eb54a984df57cff9a
Camibaza <bazante_mila@hotmail.com>
2021-12-28 17:58:22 -0300

    Atualização 28-12

---
+++ src/app/alimenticio/alimenticio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alimenticio',
+  templateUrl: './alimenticio.component.html',
+  styleUrls: ['./alimenticio.component.css']
+})
+export class AlimenticioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/moda/moda.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-moda',
+  templateUrl: './moda.component.html',
+  styleUrls: ['./moda.component.css']
+})
+export class ModaComponent implements OnInit {
+
+  constructor(
+    
+   
+
+  ) { }
+
+  ngOnInit(){
+  }
+
+
+
+}
